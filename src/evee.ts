8d9be698ac548f44f67543e8083bf97214cfb54f
Splitty <splittydev@gmail.com>
2017-02-16 16:49:10 +0100

    Initial commit
    Signed-off-by: Splitty <splittydev@gmail.com>

---
+++ src/evee.ts
@@ -0,0 +1,9 @@
+export class Evee {
+  private gid: number;
+  private receivers: Array<any>;
+
+  constructor() {
+    this.gid = 0;
+    this.receivers = [];
+  }
+}
---
+++ test/evee-spec.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="../node_modules/@types/mocha/index.d.ts" />
+
+import { Evee } from "../src/evee";
+import * as chai from "chai";
+
+const expect = chai.expect;
